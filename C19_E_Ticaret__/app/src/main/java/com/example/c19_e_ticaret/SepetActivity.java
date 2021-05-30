d4aabfe3ed0f65ae39dae5f94bddfb527011883b
İsmail Barkın Özerol <44930996+maglorelensar@users.noreply.github.com>
2021-05-30 19:12:21 +0300

    Android Studio ortamında hazırlanmış bir mobil E-ticaret uygulama projesidir. Proje de anakategoriler listesinden istenilen ürünün detayına girilip siparişi verilebilir. Siparişi verilen ürünlere sepetim bölümünden ulaşabilir. Verilen siparişin onayı, toplam tutarı ve istenilen ürünün sipariş iptali gerçekleştirilebilir.
    Android Studio ortamında hazırlanmış bir mobil E-ticaret uygulama projesidir. Proje de anakategoriler listesinden istenilen ürünün detayına girilip siparişi verilebilir. Siparişi verilen ürünlere sepetim bölümünden ulaşabilir. Verilen siparişin onayı, toplam tutarı ve istenilen ürünün sipariş iptali gerçekleştirilebilir.

---
+++ C19_E_Ticaret__/app/src/main/java/com/example/c19_e_ticaret/SepetActivity.java
@@ -0,0 +1,24 @@
+package com.example.c19_e_ticaret;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SepetActivity extends AppCompatActivity {
+    private Button urnGeriDon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sepet);
+
+        urnGeriDon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SepetActivity.this,UrunlerActivity.class));
+            }
+        });
+    }
+}
